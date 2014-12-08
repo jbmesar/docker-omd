67a340cde779384569431f01ea5f0b99f658759d
jbmesar <jbmesar@comvive.com>
2014-12-08 01:15:43 +0100

    Change of site name

--- watchdog.sh
+++ watchdog.sh
@@ -2,20 +2,21 @@
 
 SLEEP_TIME=60
 MAX_TRIES=5
+SITE="monitor"
 
 tries=0
 echo "** Starting OMD **"
-omd start
+omd start $SITE
 while /bin/true; do
   sleep $SLEEP_TIME
-  omd status | grep -q "stopped" && {
+  omd status $SITE | grep -q "stopped" && {
     if [ $tries -gt $MAX_TRIES ]; then
       echo "** ERROR: Stopped service found; aborting (after $tries tries) **"
       exit 1
     fi
     tries=$(( tries + 1 ))
     echo "** ERROR: Stopped service found; trying to start again **"
-    omd start
+    omd start $SITE
   }
 done
 
